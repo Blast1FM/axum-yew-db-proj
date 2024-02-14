53b79353bc2e5281999a1421d8bc7603c3833794
Blast <kingprostyle@yandex.ru>
2024-02-14 17:18:55 +0300

    #TODO actually separate into mods

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,7 @@
+mod db;
+mod handlers;
+mod routes;
+
 use axum::{response::IntoResponse, routing::get, Router};
 use clap::Parser;
 use std::net::{IpAddr, Ipv6Addr, SocketAddr};
@@ -7,12 +11,12 @@ use std::str::FromStr;
 #[derive(Parser, Debug)]
 #[clap(name = "server", about = "A server for our wasm project!")]
 struct Opt {
-    /// set the listen addr
-    #[clap(short = 'a', long = "addr", default_value = "127.0.0.1")]
+    /// set the listen addrk
+    #[clap(short = 'a', long = "addr", default_value = "::1")]
     addr: String,
 
     /// set the listen port
-    #[clap(short = 'p', long = "port", default_value = "8069")]
+    #[clap(short = 'p', long = "port", default_value = "8080")]
     port: u16,
 }
 
@@ -20,7 +24,7 @@ struct Opt {
 async fn main() {
     let opt = Opt::parse();
 
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new().route("/", get(hello)).route("/hi", get(hello_2));
 
     let sock_addr = SocketAddr::from((
         IpAddr::from_str(opt.addr.as_str()).unwrap_or(IpAddr::V6(Ipv6Addr::LOCALHOST)),
@@ -29,12 +33,16 @@ async fn main() {
 
     println!("listening on http://{}", sock_addr);
 
-    let listener = tokio::net::TcpListener::bind(&sock_addr).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(&sock_addr).await.expect("Unable to bind to socket");
 
-    axum::serve(listener,app).await.unwrap();
+    axum::serve(listener,app).await.expect("Unable to start server");
 
 }
 
 async fn hello() -> impl IntoResponse {
     "hello from server!"
+}
+
+async fn hello_2() -> impl IntoResponse{
+    "HELLO 2"
 }
