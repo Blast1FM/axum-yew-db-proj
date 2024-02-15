69f9d31a766c2c89aa977cb326b3acc161476abe
Blast <kingprostyle@yandex.ru>
2024-02-15 14:46:40 +0300

    docker compose created and set up

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -5,8 +5,9 @@ use crate::handlers;
 
 pub fn create_routes() -> Router
 {
+    // #TODO add cors
     Router::new()
         .route("/", get(handlers::hello))
         .route("/hello", get(handlers::hello_2))
-        
+
 }
