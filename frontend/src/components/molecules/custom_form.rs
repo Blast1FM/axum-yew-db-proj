c4767fb468adbaa389cb1e915a9f44a26b16e465
Blast <kingprostyle@yandex.ru>
2024-02-24 18:25:47 +0300

    state sort of works

--- frontend/src/components/molecules/custom_form.rs
+++ frontend/src/components/molecules/custom_form.rs
@@ -1,30 +1,36 @@
+use std::ops::Deref;
+
 use crate::components::atoms::custom_button::CustomButton;
 use crate::components::atoms::text_input::TextInput;
 use serde::{Deserialize, Serialize};
 use yew::prelude::*;
 
-#[derive(Serialize, Deserialize)]
-pub struct Query
+#[derive(Default, Clone)]
+struct Query
 {
   pub row_name: String,
-  pub regexp: String
+  pub regexp: String,
 }
 
 
 #[function_component(CustomForm)]
 pub fn custom_form() -> Html {
 
-  let query_state = use_state(|| Query{row_name:"null".to_owned(),regexp:"null".to_owned()});
+  let query_state = use_state(|| Query::default());
   let cloned_query_state = query_state.clone();
   let rowname_changed = Callback::from(move |rowname| 
   {
-    //todo fix
-    //query_state.row_name.set(value);
+    let mut data = cloned_query_state.deref().clone();
+    data.row_name = rowname;
+    cloned_query_state.set(data);
   });
 
-  let regexp_changed = Callback::from(|regexp|
+  let cloned_query_state = query_state.clone();
+  let regexp_changed = Callback::from(move |regexp|
   {
-    gloo::console::log!("Regexp changed to ", regexp);
+    let mut data = cloned_query_state.deref().clone();
+    data.regexp = regexp;
+    cloned_query_state.set(data);
   });
 
     html! {
@@ -32,6 +38,8 @@ pub fn custom_form() -> Html {
         <TextInput name="Row name" handle_onchange={rowname_changed}/>
         <TextInput name="Regexp" handle_onchange={regexp_changed} />
         <CustomButton label="Submit" />
+        <p>{"Row name: "} {&query_state.row_name}</p>
+        <p>{"Regexp: "} {&query_state.regexp}</p>
       </form>
     }
 }
